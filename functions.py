6d975f1493a39e6ca139e9568c5eb3554ed8a980
Franck <franckfongang99@gmail.com>
2024-11-03 01:48:08 -0400

    fred api

---
+++ functions.py
@@ -0,0 +1,76 @@
+import requests
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import plotly.express as px
+import urllib3
+
+
+load_dotenv()
+
+API_KEY_FRED = os.getenv('API_KEY_FRED')
+
+def get_request(url, timeout=1000):
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    try:
+        response = requests.get(url, timeout=timeout, verify=False) 
+        response.raise_for_status()
+        return response
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return ""
+
+def get_fred_series_search(text_search):
+    result = '+'.join(text_search.lower().split())
+    url = f"https://api.stlouisfed.org/fred/series/search?search_text={result}&api_key={API_KEY_FRED}&file_type=json"
+    data = get_request(url, 10)
+    if data:
+        return data.json()
+    else:
+        return None
+
+    
+def get_fred_series_observationns(text_search):
+    url = f"https://api.stlouisfed.org/fred/series/observations?series_id={text_search}&api_key={API_KEY_FRED}&file_type=json"
+    data = get_request(url, 10)
+    if data:
+        return data.json()
+    else:
+        return None
+    
+
+def get_data(text):
+    data = get_fred_series_search(text)
+    for idx, series in enumerate(data["seriess"]):
+        if not series:
+            continue
+        if idx > 5:
+            break
+        dates = get_fred_series_observationns(series["id"])
+
+        date =[]
+        value = []
+
+        for observation in dates["observations"]:
+            date_str = observation["date"] 
+            value_str = observation["value"].strip() 
+
+            if value_str and value_str != '.':
+                try:
+                    value_float = float(value_str)
+                    date.append(date_str)
+                    value.append(value_float)
+                except ValueError:
+                    print(f"Invalid value for conversion: {value_str}")
+            else:
+                print(f"Skipping invalid value: {value_str}")
+
+        df = pd.DataFrame({
+            'Date': date,
+            'Value': value,
+        })
+
+        fig = px.line(df, x='Date', y='Value', title='', markers=True)
+        fig.update_layout(xaxis_title='Date', yaxis_title='Value', xaxis_tickangle=-45)
+        
+        return fig
--- llm.py
+++ llm.py
@@ -2,31 +2,30 @@ import boto3
 import json
 
 bedrock = boto3.client(
-    service_name="bedrock-runtime",
-    region_name="uswest-2",
-    aws_access_key_id="your_access_key_id",
-    aws_secret_access_key="your_secret_access_key"
+  service_name='bedrock-runtime', 
+  region_name="us-west-2"
 )
 
-prompt = "Can you see this message"
+prompt = """
+Write a medium blog post on how to use 
+Amazon Bedrock to write an article on how to use Bedrock.
+"""
 
 body = json.dumps({
+    "anthropic_version": "bedrock-2023-05-31",
     "prompt": prompt,
-    "max_tokens": 2048,
-    "top_p": 0.8,
-    "temperature": 0.7,
+    "max_tokens": 1000,
+    "temperature": 0.75,
+    "p": 0.01,
+    "k": 0,
 })
 
-modelId = "anthropic.claude-3-sonnet-20240229-v1:0"
+modelId = 'anthropic.claude-3-sonnet-20240229-v1:0'
+accept = 'application/json'
+contentType = 'application/json'
 
-accept = "application/json"
-contentType = "application/json"
+response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
 
-response = bedrock.invoke_model(
-    body=body,
-    modelId=modelId,
-    accept=accept,
-    contentType=contentType,
-)
+response_body = json.loads(response.get('body').read())
 
-print(json.loads(response.get('body').read()))
+print(response_body['generations'][0]['text'])
